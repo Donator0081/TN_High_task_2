8839c418b82cbea5771bd8b0361568189dfb8089
ivanov.io <iliya00817@gmail.com>
2023-05-25 13:10:37 +0300

    Initial

--- src/main/java/com/example/tn_high_task_2/controllers/HighTask2RestController.java
+++ src/main/java/com/example/tn_high_task_2/controllers/HighTask2RestController.java
@@ -1,7 +1,5 @@
 package com.example.tn_high_task_2.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,28 +7,28 @@ import org.springframework.web.bind.annotation.*;
 public class HighTask2RestController {
 
     @GetMapping("{id}")
-    public ResponseEntity<String> getTask(@PathVariable int id) {
-        return new ResponseEntity<>("Get method for one Task", HttpStatus.OK);
+    public String getTask(@PathVariable int id) {
+        return "Get method for one Task";
     }
 
     @GetMapping
-    public ResponseEntity<String> getTasks() {
-        return new ResponseEntity<>("Get method for all Tasks", HttpStatus.OK);
+    public String getTasks() {
+        return "Get method for all Tasks";
     }
 
     @PostMapping
-    public ResponseEntity<String> createTask() {
-        return new ResponseEntity<>("Post method for creating Task", HttpStatus.OK);
+    public String createTask() {
+        return "Post method for creating Task";
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<String> updateTask(@PathVariable int id) {
-        return new ResponseEntity<>("Put method for updating Task", HttpStatus.OK);
+    public String updateTask(@PathVariable int id) {
+        return "Put method for updating Task";
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteTask(@PathVariable int id) {
-        return new ResponseEntity<>("Delete method for deleting Task", HttpStatus.OK);
+    public String deleteTask(@PathVariable int id) {
+        return "Delete method for deleting Task";
     }
 
 }
